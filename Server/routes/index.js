33aede6ed6116de796d02ab7aaae39deedee62c4
geovrisco <geovrisco@gmail.com>
2020-05-30 11:03:49 +0700

    add notification server

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,14 +1,27 @@
 const Router = require('express').Router()
 const articleRouter = require('./article')
 const userRouter = require('./users')
+const {handlePushTokens,saveToken} = require('../helpers/pushNotifications')
 
 Router.get('/test', (request,response) => {
     response.json('Server is Running')
 })
 
-// Router.use('/articles',articleRouter)
+
 Router.use('/users', userRouter)
 Router.use('/articles',articleRouter)
+
+Router.post('/token',(request, response) =>{
+  saveToken(request.body.token.value);
+  console.log('received token',request.body.token.value)
+  response.send('received push token',request.body.token.value)
+})
+
+Router.post('/message',(request,response) =>{
+  handlePushTokens(request.body)
+  console.log('received message',request.body.title)
+  response.send('received push token',request.body.title)
+})
 
 
 
